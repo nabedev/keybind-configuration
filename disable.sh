9f7adb00f74220e3b675cfd456052bae11b80b13
wtnb93 <wtnb93@users.noreply.github.com>
2019-06-22 21:53:17 +0900

    diable launch app keybind

--- disable.sh
+++ disable.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Disable Super_L
 gsettings set org.gnome.mutter overlay-key ''
 
@@ -8,5 +10,17 @@ gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.shell.keybindings toggle-message-tray ['']
 
 # Disable Super + a
-gsettings set org.gnome.shell.keybindings toggle-overview ''
+gsettings set org.gnome.shell.keybindings toggle-overview ['']
 gsettings set org.gnome.shell.keybindings toggle-application-view []
+
+# Disable Super + Shift + [1-9]
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-1 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-2 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-3 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-4 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-5 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-6 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-7 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-8 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-9 ['']
+gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-10 ['']
