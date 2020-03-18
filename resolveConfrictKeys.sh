8b9c3ab8c28128dc1428435ccda91818751520cd
wtnb93 <wtnb93@users.noreply.github.com>
2020-03-18 17:14:48 +0900

    update gsettings

--- resolveConfrictKeys.sh
+++ resolveConfrictKeys.sh
@@ -20,3 +20,15 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up ['']
 # Spotlight feature replaces with Activities
 gsettings set org.gnome.shell.keybindings toggle-overview "['LaunchA', '<Control>space']"
 gsettings set org.gnome.shell.keybindings toggle-application-view "['LaunchB']"
+
+# Extra
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>10']"
